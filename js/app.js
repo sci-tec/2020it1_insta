95ca0203170f822d4405d85f6d74aadfe8bbc750
rI2020-a <2020it07@sci-tec.ac.jp>
2020-12-23 13:45:36 +0900

    撮影した写真を投稿できるようにした

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
+const video  = document.querySelector("#camera");
+
 window.onload = () => {
-    const video  = document.querySelector("#camera");
-    const canvas = document.querySelector("#picture");
+
+    // const canvas = document.querySelector("#picture");
     const se     = document.querySelector('#se');
   
     /** カメラ設定 */
@@ -27,23 +29,7 @@ window.onload = () => {
     .catch( (err) => {
       console.log(err.name + ": " + err.message);
     });
-  
-    /**
-     * シャッターボタン
-     */
-     document.querySelector("#shutter").addEventListener("click", () => {
-      const ctx = canvas.getContext("2d");
-  
-      // 演出的な目的で一度映像を止めてSEを再生する
-      video.pause();  // 映像を停止
-      se.play();      // シャッター音
-      setTimeout( () => {
-        video.play();    // 0.5秒後にカメラ再開
-      }, 500);
-  
-      // canvasに画像を貼り付ける
-      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    });
+
   };
 
   $(function () {
@@ -55,6 +41,43 @@ window.onload = () => {
     })
 
     $('#mat').hide();
-    
+
+    $("#shutter").click(function(){
+      let r = addItem()
+      const canvas = document.querySelector(`#picture${r}`);
+
+      const ctx = canvas.getContext("2d");
+  
+      // 演出的な目的で一度映像を止めてSEを再生する
+      video.pause();  // 映像を停止
+      se.play();      // シャッター音
+      setTimeout( () => {
+        video.play();    // 0.5秒後にカメラ再開
+      }, 500);
+  
+      // canvasに画像を貼り付ける
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+
+      $('#mat').hide();
+    })
+
+
 
 });
+
+function addItem(img){
+  console.log("addItem");
+  let r = Math.floor(Math.random()*1000000);
+  let tag=`<div class="post">
+  <div>
+  <canvas id="picture${r}" class="thumbnail" width="300" height="200"></canvas>
+  </div> 
+  <div class="postFooter">
+      <div><img class="imgIcon" src="images/simple_heart.png"></div>
+      <div><img class="imgIcon" src="images/komento.ikon.png"></div>
+  </div>
+</div>`;
+  $("#main").append(tag);
+  return r;
+}
